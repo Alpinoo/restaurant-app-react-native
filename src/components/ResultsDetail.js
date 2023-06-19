7dc52db54f420705bb5218f35d2e2c77e0ebc733
Alp Oral <50188463+Alpinoo@users.noreply.github.com>
2023-06-19 15:12:34 +0300

    added image added ratings and review count

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -4,11 +4,22 @@ import { View,Image,StyleSheet,Text } from "react-native";
 const ResultsDetail=({result})=>{
     return(
         <View>
-            <Text>{result.name}</Text>
+        <Image style={styles.image} source={{uri: result.image_url}}/>
+            <Text style={styles.text}>{result.name}</Text>
+            <Text>{result.rating} Ratings, {result.review_count} Reviews</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image:{//?without width and heigh, image won't be shown as default
+        width:250,
+        height:125,
+        borderRadius:3 //remove edges from image
+    },
+    text:{
+        fontWeight:'bold'
+    }
+})
 
 export default ResultsDetail
